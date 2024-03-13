ede41c0e68cdc8ad6997e5167fa298ec385776b7
Lesha <leshapowerlifter@gmail.com>
2024-03-13 14:49:55 +0200

    Added better ui

--- src/App.jsx
+++ src/App.jsx
@@ -8,20 +8,32 @@ function App() {
   const [currentPage, setCurrentPage] = useState(0);
   const [filteredWords, setFilteredWords] = useState([]);
   const [totalPages, setTotalPages] = useState(0);
+  const [wordsWithLetter, setWordsWithLetter] = useState(0);
+  const [currentLetter, setCurrentLetter] = useState(null);
 
   const filterWords = (letter) => {
     const filtered = sameWords.filter((word) => word.startsWith(letter));
     setFilteredWords(filtered);
+    setCurrentLetter(letter);
     setCurrentPage(0); // Reset the current page to 0 when filtering
+    setWordsWithLetter(filtered.length);
     setTotalPages(Math.ceil(filtered.length / wordsPerPage)); // Calculate total pages
   };
 
   const displayWords = () => {
     const startIndex = currentPage * wordsPerPage;
     const endIndex = startIndex + wordsPerPage;
-    return filteredWords
-      .slice(startIndex, endIndex)
-      .map((word, index) => <div key={index}>{word}</div>);
+    const wordsToDisplay = filteredWords.slice(startIndex, endIndex);
+
+    return (
+      <div className="word-grid">
+        {wordsToDisplay.map((word, index) => (
+          <div key={index} className="word-item">
+            {word}
+          </div>
+        ))}
+      </div>
+    );
   };
 
   const nextPage = () => {
@@ -39,6 +51,7 @@ function App() {
   return (
     <div className="container">
       <h1>The same words in Russian and Bulgarian</h1>
+
       <div className="alphabet">
         {Array.from(Array(32).keys()).map((letterIndex) => (
           <button
@@ -50,37 +63,51 @@ function App() {
           </button>
         ))}
       </div>
-      <div className="sameWords-container">{displayWords()}</div>
+      <div className="word-count">
+        {wordsWithLetter > 0 ? (
+          <span>
+            {wordsWithLetter} words start with {currentLetter?.toUpperCase()}
+          </span>
+        ) : (
+          currentLetter && (
+            <span>
+              There aren't words start with {currentLetter.toUpperCase()}
+            </span>
+          )
+        )}
+      </div>
+      <div className="words-container">{displayWords()}</div>
+
       <div className="pagination">
         <button
           onClick={prevPage}
           disabled={currentPage === 0}
           style={{
-            borderTopLeftRadius: "10px", // Radius for the top-left corner
-            borderBottomLeftRadius: "10px", // Radius for the bottom-left corner
-            borderTopRightRadius: "0", // No radius for the top-right corner
-            borderBottomRightRadius: "0", // No radius for the bottom-right corner
+            borderTopLeftRadius: "10px",
+            borderBottomLeftRadius: "10px",
+            borderTopRightRadius: "0",
+            borderBottomRightRadius: "0",
           }}
         >
           Previous
         </button>
         {totalPages > 0 && (
           <div className="page-count">
-            <hr className="line" /> {/* Line above */}
+            <hr className="line" />
             <span>
               {currentPage + 1} of {totalPages}
             </span>
-            <hr className="line" /> {/* Line below */}
+            <hr className="line" />
           </div>
         )}
         <button
           onClick={nextPage}
           disabled={(currentPage + 1) * wordsPerPage >= filteredWords.length}
           style={{
-            borderTopLeftRadius: "0", // Radius for the top-left corner
-            borderBottomLeftRadius: "0", // Radius for the bottom-left corner
-            borderTopRightRadius: "10px", // No radius for the top-right corner
-            borderBottomRightRadius: "10px", // No radius for the bottom-right corner
+            borderTopLeftRadius: "0",
+            borderBottomLeftRadius: "0",
+            borderTopRightRadius: "10px",
+            borderBottomRightRadius: "10px",
           }}
         >
           Next
